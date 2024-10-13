68da4999d2a57465c2248aa642e6c07df301af62
rhk998 <130037816+rhk998@users.noreply.github.com>
2024-10-14 03:27:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	const lowerS1 = s1.toLowerCase();
+    const lowerS2 = s2.toLowerCase();
+    
+    // Use the indexOf method to find the first occurrence of lowerS2 in lowerS1
+    return lowerS1.indexOf(lowerS2);
 }
 
 // Please do not change the code below
